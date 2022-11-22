8d7e349a5d2ee72a216c29beadcffd23fee65246
PZubko <pts1701@gmail.com>
2022-11-22 16:03:38 +0300

    add static ant url names

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     # path('about', views.about),
-    path('item/<int:item_id>', views.item_id),
-    path('items', views.items_list),
+    path('item/<int:item_id>', views.item_id, name="item-detail"),
+    path('items', views.items_list, name="items-list"),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
