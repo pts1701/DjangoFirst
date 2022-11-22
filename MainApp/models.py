7d574d39f1a8b541822f132aa200e53da245048b
PZubko <pts1701@gmail.com>
2022-11-22 14:15:28 +0300

    Module 2 and 3 practice migration 03

--- MainApp/models.py
+++ MainApp/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
-    info = models.CharField(max_length=150, default="description")
+    info = models.TextField(max_length=2000, default="description")
 
     def __repr__(self):
         return f"Item: {self.name} {self.brand} count:{self.count} описание:{self.info}"
